2de82743d6f4bafc8fd94bc8b41f50d9d0dc055d
Idris <nox_77@mail.ru>
2025-08-13 18:57:00 +0300

    Change ftracker.go

--- ftracker.go
+++ ftracker.go
@@ -134,5 +134,5 @@ func swimmingMeanSpeed(lengthPool, countPool int, duration float64) float64 {
 // weight float64 — вес пользователя.
 func SwimmingSpentCalories(lengthPool, countPool int, duration, weight float64) float64 {
 	// ваш код здесь
-	return (meanSpeed(lengthPool, float64(countPool)) + swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight / duration
+	return (swimmingMeanSpeed(lengthPool, countPool, duration) + swimmingCaloriesMeanSpeedShift) * swimmingCaloriesWeightMultiplier * weight / duration
 }
